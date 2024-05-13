5288adea38eb1e9c89b352bdb216ed745085bd05
Islom Toirjonov <toirjonovislomjon3@gmail.com>
2024-05-13 11:45:28 +0500

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+let sonda = Number(prompt("Hafta kunini sonda kiriting"));
+
+switch (sonda) {
+    case 1: alert("Dushanba"); break;
+    case 2: alert("Seshanba"); break;
+    case 3: alert("Chorshanba"); break;
+    case 4: alert("Payshanba"); break;
+    case 5: alert("Juma"); break;
+    case 6: alert("Shanba"); break;
+    case 7: alert("Yakshanba"); break;
+}
